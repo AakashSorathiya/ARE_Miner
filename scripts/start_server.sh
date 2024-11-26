eff390e95abc6444355c5a97ce2724b19374f6bb
Aakash Sorathiya <akashsorthiya@gmail.com>
2024-11-26 09:15:44 -0700

    update script

--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
-# sudo supervisorctl stop fastapiapp
-# sudo kill -s SIGTERM $(sudo supervisorctl pid)
+sudo kill -s SIGTERM $(pgrep -f 'supervisord')
 cd /home/ubuntu/ARE_Miner
 source environment/bin/activate
 sudo supervisord -c supervisord.conf
